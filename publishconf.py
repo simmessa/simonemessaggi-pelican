f9ba7e52e4d5c3d0241773c2b39cb3e9e9c4d1f9
Simone Messaggi <simmessa@gmail.com>
2025-08-22 17:01:04 +0200

    Making some fixes on domain name

--- publishconf.py
+++ publishconf.py
@@ -8,7 +8,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "https://simmessa.github.io/simonemessaggi-pelican"
+SITEURL = "https://www.simonemessaggi.it" # https://simmessa.github.io/simonemessaggi-pelican"
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
